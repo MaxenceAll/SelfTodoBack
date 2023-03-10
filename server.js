54e2de9b10c1c2c9c8b392f3271aaab3b6d1ae69
MaxenceAll <maxence.allart@gmail.com>
2023-03-10 01:34:44 +0100

    finished

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const cors = require("cors");
 app.use(cors());
 //pour recevoir des json il faut ca :)
 app.use(express.json());
+// import bcrypt pour le password
+const bcrypt = require("bcrypt");
+// import jsonwebtoken
+const jwt = require("jsonwebtoken");
 
 //import des requetes
 const { query } = require("./services/database.service");
@@ -59,17 +63,60 @@ app.put("/todos/:id", async (req, res) => {
 
 // delete a todo by id
 app.delete("/todos/:id", async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   try {
-    await query(
-      'DELETE FROM todos WHERE id = ?',
-      [id]);
+    await query("DELETE FROM todos WHERE id = ?", [id]);
     res.sendStatus(200);
   } catch (error) {
     console.error(error);
   }
 });
 
+// sign up route
+app.post("/signup", async (req, res) => {
+  console.log("Sign up route taken");
+  const { email, password } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  try {
+    await query(`INSERT INTO users (email, hashed_password) VALUES(?,?)`, [
+      email,
+      hashedPassword,
+    ]);
+    const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
+    res.json({ email, token });
+  } catch (error) {
+    console.error(error);
+    if (error) {
+      res.json({ detail: error.sqlMessage });
+    }
+  }
+});
+
+// login route
+app.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+  try {
+    const users = await query(`SELECT * FROM users WHERE email = ?`, [email]);
+    console.log(users);
+    if (!users[0] || !users){
+      return res.json({ detail: 'User does not exists !(TODO not secure)'})
+    }else{
+      //compare le pass recu avec le hashed pass via bcrypt
+      const success = await bcrypt.compare(password, users[0].hashed_password)
+      // login ok donc creation d'un token
+      const token = jwt.sign( {email} , 'secret' , {expiresIn: '1hr'} );
+      if (success) {
+        //si compare ok on renvoi un json avec le mail et le token
+        res.json( {'email': users[0].email, token} )
+      }else{
+        res.json( {detail: "Login fail, check email or password"})
+      }
+    }    
+  } catch (error) {
+    console.error(error)
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
